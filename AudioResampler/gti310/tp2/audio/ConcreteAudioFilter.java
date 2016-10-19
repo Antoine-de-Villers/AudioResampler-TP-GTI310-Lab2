4cb1513377152d97ab72f99e2f38b81f16021259
frk10 <frk10@DESKTOP-D4MB2D6>
2016-10-19 16:12:07 -0400

    test

---
+++ AudioResampler/gti310/tp2/audio/ConcreteAudioFilter.java
@@ -0,0 +1,27 @@
+package gti310.tp2.audio;
+
+import gti310.tp2.io;
+
+public class ConcreteAudioFilter implements AudioFilter {
+
+	FileSource reader;
+	FileSink writer;
+	
+	
+	public ConcreteAudioFilter(String fichier) {
+		// TODO Auto-generated constructor stub
+		
+		
+	}
+	
+	@Override
+	public void process() {
+		// TODO Auto-generated method stub
+		read();
+	}
+	
+	private void read(){
+		
+		
+	}
+}
